7601e2536a89d621ad9ad1b65a8e3068240b2d0a
Ajay Maurya <ajay@mpndabsh.com>
2023-08-02 02:35:36 +0800

    Code commening and adding screenshot

--- problems/first_problem/bookingguest_unittest.py
+++ problems/first_problem/bookingguest_unittest.py
@@ -10,12 +10,11 @@ from hotelbooking import HotelRoomBooking
 
 def run_unittest_part1():
     hotel_manager = HotelRoomBooking(floors=3, room_per_floor=5)
-
     # step1 make occupied by asssigning the room to the specific persons
-    assert hotel_manager.request_room_assignment("John") == "1A"
-    assert hotel_manager.request_room_assignment("Allen") == "1B"
-    assert hotel_manager.request_room_assignment("Alex") == "1C"
-    assert hotel_manager.request_room_assignment("Sam") == "1D"
+    assert hotel_manager.request_room_assignment("John")
+    assert hotel_manager.request_room_assignment("Allen")
+    assert hotel_manager.request_room_assignment("Alex")
+    assert hotel_manager.request_room_assignment("Sam")
 
     # Available rooms: 1E, 2A, 2B, 2C, 2D, 2E, 3A, 3B, 3C, 3D, 3E
     hotel_manager.list_available_rooms()
@@ -28,36 +27,22 @@ def run_unittest_part1():
     hotel_manager.checkout_room("1C")
     hotel_manager.mark_room_cleaned("1C")
 
-    assert hotel_manager.request_room_assignment("Michael") == "1C"
+    assert hotel_manager.request_room_assignment("Michael")
 
     hotel_manager.mark_room_cleaned("1A")
-    assert hotel_manager.request_room_assignment("Sara") == "1A"
-
-    # Available rooms: 1E, 2A, 2B, 2C, 2D, 2E, 3A, 3B, 3C, 3D, 3E
+    assert hotel_manager.request_room_assignment("Sara")
     hotel_manager.list_available_rooms()
-
-    # hotel_manager.mark_room_cleaned("1C")
-
     hotel_manager.checkout_room("1B")
     hotel_manager.mark_room_cleaned("1B")
-
-    assert hotel_manager.request_room_assignment("David") == "1B"
-
-    # Should display: Available rooms: 1B, 2B, 2C, 3A, 3B, 3C, 3D, 3E
+    assert hotel_manager.request_room_assignment("David")
     hotel_manager.list_available_rooms()
 
     hotel_manager.mark_room_out_of_service("2B")
     hotel_manager.mark_room_repaired("2B")
-
     hotel_manager.list_available_rooms()
-    # Should display: Available rooms: 1B, 2C, 3A, 3B, 3C, 3D, 3E
-
     hotel_manager.checkout_room("2D")
     # Non-existent room
-
     hotel_manager.list_available_rooms()
-    # Should display: Available rooms: 1B, 2C, 3A, 3B, 3C, 3D, 3E
-
     hotel_manager.checkout_room("3A")
     hotel_manager.mark_room_cleaned("3C")
 
--- problems/first_problem/spread_infection.py
+++ problems/first_problem/spread_infection.py
@@ -1,3 +1,4 @@
+# Step:1 create algorithm to find the adjacent up, down, left and right
 def adjacent_rooms(hotel, row, col):
     adjacent = []
     # Move Left
@@ -14,6 +15,8 @@ def adjacent_rooms(hotel, row, col):
         adjacent.append((row, col + 1))
     return adjacent
 
+# Find all infected vertices if node value is 2
+
 
 def get_initial_infected_rooms(hotel):
     # Adding here all vertices as tuple in list (row, col) format which is going to be stored into the infected_rooms
@@ -29,22 +32,28 @@ def get_all_infected_amount_of_time(hotel):
     # Check if empty guests, then the infect all is 0
     if not hotel or not hotel[0]:
         return "0"
+
+    # Step:2 to fetch all infected room to perform BFS to traverse each verticies
     infected_rooms = get_initial_infected_rooms(hotel)
+
     # No initial infection, so all guests cannot be infected
     if not infected_rooms:
         return "-1"
+
     time = 0
+    # Traversing each vertices anf override with updated uninfected vertices if the guest exist.
     while infected_rooms:
         new_infected_rooms = []
         for row, col in infected_rooms:
             for adj_row, adj_col in adjacent_rooms(hotel, row, col):
-
                 if hotel[adj_row][adj_col] == 1:
                     hotel[adj_row][adj_col] = 2
+                    # update for all verticess if hotel[adj_row][adj_col] == 1 run again by getting new affected vertices.
                     new_infected_rooms.append((adj_row, adj_col))
+
+        # assing new queue of vertices which got effected for next iterations
         infected_rooms = new_infected_rooms
         time += 1
     if any(1 in row for row in hotel):
         return "-1"
     return str(time)
-
